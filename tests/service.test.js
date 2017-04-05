4523ce5479192fd284df42aaa558bb98a821597e
k1r0s <ciroreed@gmail.com>
2017-04-05 09:33:35 +0200

    initial version

---
+++ tests/service.test.js
@@ -0,0 +1,8 @@
+var lib = require("../lib/index");
+
+// describe("dms service allows to easily integrate virtual-dom to our projects", function(){
+//   it("", function(){})
+//   it("", function(){})
+//   it("", function(){})
+//   it("", function(){})
+// })
